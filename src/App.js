45fecd1ffe8f09322391caa424f87d0ee0d1413e
Sam White <samuelwhite98@outlook.com>
2023-04-13 23:44:12 +0100

    Separate form states

--- src/App.js
+++ src/App.js
@@ -4,82 +4,120 @@ import { Education } from "./components/Education";
 import { Preview } from "./components/Preview";
 import { Work } from "./components/Work"
 import "./styles/App.css"
+import uniqid from "uniqid";
 
 class App extends Component {
     constructor(){
         super();
 
         this.state = {
-            fields: {
+            aboutFields: {
                 inputName: "Name",
                 inputEmail: "Email",
                 inputTelephone: "Telephone",
+                id: uniqid()
+            },
+            educationFields: {
                 inputSchoolName: "School Name",
                 inputTitle: "Title of Study",
                 inputDate: "Date",
+                id: uniqid()
+            },
+            workFields: {
                 inputCompany: "Company Name",
                 inputPositionTitle: "Position Title",
                 inputTasks: "Tasks",
                 inputDateFrom: new Date().toISOString().split('T')[0],
-                inputDateTo: new Date().toISOString().split('T')[0]
-            }
+                inputDateTo: new Date().toISOString().split('T')[0],
+                id: uniqid()
+            },
+            educationForms: [],
+            workForms: []
         }
-
-        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleChange = (e) => {
+    handleAboutChange = (e) => {
 
         const {name, value} = e.target
-        console.log(this.state.fields.inputName)
 
         this.setState(prevState => ({
-            fields: {
-                ...prevState.fields,
+            aboutFields: {
+                ...prevState.aboutFields,
                 [name]: value
 
             }
         }));
     };
 
+    handleEducationChange = (e) => {
+
+        const {name, value} = e.target
+
+        this.setState(prevState => ({
+            educationFields: {
+                ...prevState.educationFields,
+                [name]: value
+
+            }
+        }));
+    };
+
+    handleWorkChange = (e) => {
+
+        const {name, value} = e.target
+
+        this.setState(prevState => ({
+            workFields: {
+                ...prevState.workFields,
+                [name]: value
+
+            }
+        }));
+    };
+
+    addEducationForm = () => {
+        
+    }
+
+
     render(){
         return(
             <div className="container">
                 <div className="inputContainer">
                     <About 
-                        inputName={this.state.fields.inputName}
-                        inputEmail={this.state.fields.inputEmail}
-                        inputTelephone={this.state.fields.inputTelephone}
-                        handleChange={this.handleChange}
+                        inputName={this.state.aboutFields.inputName}
+                        inputEmail={this.state.aboutFields.inputEmail}
+                        inputTelephone={this.state.aboutFields.inputTelephone}
+                        handleChange={this.handleAboutChange}
                     />
                     <Education
-                        inputSchoolName={this.state.fields.inputSchoolName}
-                        inputTitle={this.state.fields.inputTitle}
-                        inputDate={this.state.fields.inputDate}
-                        handleChange={this.handleChange}
+                        inputSchoolName={this.state.educationFields.inputSchoolName}
+                        inputTitle={this.state.educationFields.inputTitle}
+                        inputDate={this.state.educationFields.inputDate}
+                        handleChange={this.handleEducationChange}
                     />
                     <Work
-                        inputCompany={this.state.fields.inputCompany}
-                        inputPositionTitle={this.state.fields.inputPositionTitle}
-                        inputTasks={this.state.fields.inputTasks}
-                        inputDateFrom={this.state.fields.inputDateFrom}
-                        inputDateTo={this.state.fields.inputDateTo}
-                        handleChange={this.handleChange}
+                        inputCompany={this.state.workFields.inputCompany}
+                        inputPositionTitle={this.state.workFields.inputPositionTitle}
+                        inputTasks={this.state.workFields.inputTasks}
+                        inputDateFrom={this.state.workFields.inputDateFrom}
+                        inputDateTo={this.state.workFields.inputDateTo}
+                        handleChange={this.handleWorkChange}
                     />
                 </div>
                 <div className="previewContainer">
                     <Preview
-                        inputName={this.state.fields.inputName}
-                        inputEmail={this.state.fields.inputEmail}
-                        inputTelephone={this.state.fields.inputTelephone}
-                        inputSchoolName={this.state.fields.inputSchoolName}
-                        inputTitle={this.state.fields.inputTitle}
-                        inputDate={this.state.fields.inputDate}
-                        inputCompany={this.state.fields.inputCompany}
-                        inputPositionTitle={this.state.fields.inputPositionTitle}
-                        inputTasks={this.state.fields.inputTasks}
-                        inputDateFrom={this.state.fields.inputDateFrom}
-                        inputDateTo={this.state.fields.inputDateTo}
+                        inputName={this.state.aboutFields.inputName}
+                        inputEmail={this.state.aboutFields.inputEmail}
+                        inputTelephone={this.state.aboutFields.inputTelephone}
+                        inputSchoolName={this.state.educationFields.inputSchoolName}
+                        inputTitle={this.state.educationFields.inputTitle}
+                        inputDate={this.state.educationFields.inputDate}
+                        inputCompany={this.state.workFields.inputCompany}
+                        inputPositionTitle={this.state.workFields.inputPositionTitle}
+                        inputTasks={this.state.workFields.inputTasks}
+                        inputDateFrom={this.state.workFields.inputDateFrom}
+                        inputDateTo={this.state.workFields.inputDateTo}
                     />
                 </div>
             </div>
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -10,7 +10,23 @@ class Preview extends Component{
     render(){
         return(
             <div>
-                <h1>{this.props.inputName}</h1>
+                <div className="aboutInfo">
+                    <h1>{this.props.inputName}</h1>
+                    <h2>{this.props.inputEmail}</h2>
+                    <h3>{this.props.inputTelephone}</h3>
+                </div>
+                <div className="educationInfo">
+                    <h2>Education</h2>
+                    <p>{this.props.inputDate}</p>
+                    <p>{this.props.inputSchoolName}: {this.props.inputTitle}</p>
+                </div>
+                <div className="workInfo">
+                    <h2>Work Experience</h2>
+                    <p>{this.props.inputCompany}: {this.props.inputDateFrom} - {this.props.inputDateTo}</p>
+                    <p>{this.props.inputPositionTitle}</p>
+                    <p>{this.props.inputTasks}</p>
+                </div>
+                
             </div>
         )
     }
